4dab69733f433d3f660393d6f88703275175dcfd
alphastryke <88189174+alphastryke@users.noreply.github.com>
2022-08-18 20:07:07 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,6 @@ var market_url = "http://127.0.0.1:8080/market";
 var market_array = [];
 var marketCount = 0;
 var category = "All Markets"
+
+var translate_url = new URL("https://pohsxa8827.execute-api.us-east-1.amazonaws.com/translate")
+var newText = "";
